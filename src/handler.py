a195bac4fb93e5cf444e8eba4875c1b3950bf9c3
Artem Durynin <60298175+Artem535@users.noreply.github.com>
2026-02-06 00:13:42 +0300

    Refactor engine initialization in handler.py
    Refactor handler to initialize engines only in main block.

--- src/handler.py
+++ src/handler.py
@@ -3,20 +3,27 @@ import runpod
 from utils import JobInput
 from engine import vLLMEngine, OpenAIvLLMEngine
 
-vllm_engine = vLLMEngine()
-OpenAIvLLMEngine = OpenAIvLLMEngine(vllm_engine)
+vllm_engine = None
+openai_engine = None
 
 async def handler(job):
     job_input = JobInput(job["input"])
-    engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
+    engine = openai_engine if job_input.openai_route else vllm_engine
     results_generator = engine.generate(job_input)
     async for batch in results_generator:
         yield batch
 
-runpod.serverless.start(
-    {
-        "handler": handler,
-        "concurrency_modifier": lambda x: vllm_engine.max_concurrency,
-        "return_aggregate_stream": True,
-    }
-)
+if __name__ == "__main__":
+    if vllm_engine is None:
+        vllm_engine = vLLMEngine()
+
+    if openai_engine is None:
+        openai_engine = OpenAIvLLMEngine(vllm_engine)
+    
+    runpod.serverless.start(
+        {
+            "handler": handler,
+            "concurrency_modifier": lambda x: vllm_engine.max_concurrency,
+            "return_aggregate_stream": True,
+        }
+    )
